8cb333fe7fa083eda6b3f6cfbbb8d9d90d3a74e7
Jonatas Alves <143411776+JonatasAlv3s@users.noreply.github.com>
2025-04-05 12:31:10 -0300

    finish home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,21 @@ export default function Home() {
 					<span className="dollar">$</span>
 					<span className="text-initial">init portfolio</span>
 				</div>
-
+				<h1 className="name">Jonatas Alves</h1>
+				<div className="code-text">
+					<span className="initial-text">const</span> developer = {"{"}
+				</div>
+				<div className="code-text">
+					<span className="initial-text">skills:</span>
+					<span className="string">&quot;Front-end Developer&quot;</span>,
+				</div>
+				<div className="code-text">
+					<span className="initial-text">passion: </span>
+					<span className="string">&quot;Criando belas experiências na Web&quot;</span>,
+				</div>
+				<div>
+					<span className="code-text">{"};"}</span>
+				</div>
 			</div>
 		</div>
 
